d6893be6246455b0790115d858aa303929046bf3
vic778 <victoiremmanuelbarh@gmail.com>
2022-03-25 16:54:11 +0300

    change root

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
-  resources :line_items
-  resources :carts
-  resources :instruments
+
   devise_for :users, controllers: {
     registrations: 'registrations'
   }
-  root 'instruments#index'
+
+  resources :line_items
+  resources :carts
+  resources :instruments
+ 
+  root to: 'instruments#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
